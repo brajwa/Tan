96b060f1fcdae92263ad156e2833357faaec7a0a
brajwa <brajwa@purdue.edu>
2018-11-16 15:07:45 -0500

    RStudio

--- Tan_stats.R
+++ Tan_stats.R
@@ -12,3 +12,13 @@ require(emmeans)
 require(readxl)
 library(httr)
 require(ggfortify)
+
+#####################################################################################
+#### Get the data from Excel
+
+
+spatial.data <- read_excel("/Users/rajwa/Documents/Revolution/Powley/Tan/Tan-Logan Data Spreadsheet_100318.xlsx", sheet=1)
+
+as.matrix(colnames(spatial.data))
+
+deg2rad <- function(deg) {(deg * pi) / (180)}
