d9e867fa292751618a6c57f5489196211a4de37a
brajwa <brajwa@purdue.edu>
2018-11-16 14:07:05 -0500

    RStudio

---
+++ Tan_stats.R
@@ -0,0 +1,14 @@
+#### keywords: spatial statistics, SPARC, Powley, K-function, Kashif
+
+#####################################################################################
+#####################################################################################
+####### needed libraries
+
+require(tibble)
+require(magrittr)
+require(dplyr)
+require(multcomp)
+require(emmeans)
+require(readxl)
+library(httr)
+require(ggfortify)
